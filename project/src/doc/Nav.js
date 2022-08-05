b28874e10e903e279499c55b5211f4e994eb83b7
kimwoojin <kimwoojin08@naver.com>
2022-08-05 16:20:05 +0900

    20220805

--- project/src/doc/Nav.js
+++ project/src/doc/Nav.js
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className="Nav gnb">
             <ul>
-                <li><Link to="/section01">main</Link></li>
+                <li><Link to="/">main</Link></li>
                 <li><Link to="/section01">sc01</Link></li>
                 <li><Link to="/section02">sc02</Link></li>
                 <li><Link to="/section03">sc03</Link></li>
---
+++ project/src/doc/Section04.js
@@ -0,0 +1,71 @@
+const Section04 = () => {
+    return (
+        <section className='About02 basicSection'>
+            <h2>CEO's<span>GREETINGS</span></h2>
+            <p>항상 새로운 포카리스웨트</p>
+            <div className="inner">
+                <div className="top">
+                    <figure>
+                        <img src="./img/main01.jpg" alt="" />
+                    </figure>
+
+                </div>
+                <div className="left">
+                    <p className="tit">
+                        <strong>상큼함이 터지는 포도맛 코코팜</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, a.</p>
+                    </p>
+                </div>
+                <div className="right">
+                    <div className="des">
+                        <p className="tit">
+                            웹서비스의 <span className="mainColor">새로운 미래를</span><br />
+                            <span className="mainColor">코코팜</span>이 만들어 가겠습니다.
+                        </p>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br /> Voluptates deserunt nulla modi,
+                            autem velit explicabo eaque ea quis possimus vitae?
+                        </p>
+
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br /> Aperiam qui doloribus voluptates
+                            voluptatibus ipsa beatae voluptas nulla dolorum nihil expedita, tenetur impedit dolorem quasi rem?
+                        </p>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br /> Voluptate, veniam repellat voluptates
+                            minima facere necessitatibus.</p>
+
+                        <p>코코팜 임직원 일동</p>
+
+                    </div>
+                    50%
+                </div>
+            </div>
+            <div className="inner">
+                <h3>주요현황</h3>
+                <div className="responsive-table">
+                    <table className="table">
+
+                        <tbody>
+                            <tr>
+                                <th>주 소</th>
+                                <td>부산시 동래구 사직동 1234-56
+                                    포카리스웨터 옆
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <th>전화번호 </th>
+                                <td>051-783-2317</td>
+
+
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section04;
